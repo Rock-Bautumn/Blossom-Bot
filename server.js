bca42139440b860cc2c4b1a7c9b6f0870e62aea6
Rock-Bautumn <3623@holbertonschool.com>
2022-04-16 22:18:30 -0500

    Adding complete image and channelviewer api methods in server.js

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 #!/usr/bin/node
 
 const express = require('express')
+const mysql = require('mysql2');
+
 const app = express()
-const port = 5002
+const expressport = 5002
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "blossom_dev",
+  password: "blossom_dev_pwd"
+});
+
+
 
 var shutting_down = false;
+
 
 app.use(function (req, resp, next) {
  if(!shutting_down)
@@ -37,15 +48,98 @@ process.on('SIGTERM', cleanup);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// get plant image by level no
 app.get('/images/:imageNo', (req, res) => {
   console.log(req.params)
   imageNo = req.params.imageNo
-  if (!(imageNo >= 0) || !(imageNo <= 5)) { resp.send(400, "Bad value, flower must be between 0-5"); }
+  if (!(imageNo >= 0) || !(imageNo <= 5)) { res.send(400, "Bad value, flower must be between 0-5"); }
   const filepath = __dirname + '/images/level_' + imageNo + '_flower_in_pot.png'
   res.sendFile(filepath);
 })
 
-var server = app.listen(port, () => {
+// get view time for chan viewer
+app.get('/api/viewtime/:channelname/:username', (req, res) => {
+  con.connect(function(err) {
+    if (err) throw err;
+    const sqlquery = "SELECT viewing_time FROM ChannelViews WHERE channelname = '" + req.params.channelname + "' AND username = '" + req.params.username + "';"
+    con.query(sqlquery, function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+      if (JSON.stringify(result) !== '[]') { res.send(result[0]) }
+      else { res.send(404, "No results"); }
+    });
+  });
+});
+
+// create new chanviewer
+app.post('/api/viewtime/:channelname/:username', (req, res) => {
+  con.connect(function(err) {
+    if (err) throw err;
+    const sqlquery = `INSERT INTO ChannelViews (channelname, username) VALUES ('${req.params.channelname}', '${req.params.username}');`;
+    con.query(sqlquery, function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+      if (JSON.stringify(result) !== '[]') { res.send(result) }
+      else { res.send(404, "No results"); }
+    });
+  });
+});
+
+// update chanview with new viewing time
+app.put('/api/viewtime/:channelname/:username/:viewing_time', (req, res) => {
+  con.connect(function(err) {
+    if (err) throw err;
+    const sqlquery = `UPDATE ChannelViews SET viewing_time = ${req.params.viewing_time} WHERE channelname = '${req.params.channelname}' AND username = '${req.params.username}';`;
+    con.query(sqlquery, function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+      if (JSON.stringify(result) !== '[]') { res.send(result) }
+      else { res.send(404, "No results"); }
+    });
+  });
+});
+
+// delete chanviewer
+app.delete('/api/viewtime/:channelname/:username/', (req, res) => {
+  con.connect(function(err) {
+    if (err) throw err;
+    const sqlquery = `DELETE from ChannelViews WHERE channelname = '${req.params.channelname}' AND username = '${req.params.username}';`;
+    con.query(sqlquery, function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+      if (JSON.stringify(result) !== '[]') { res.send(result) }
+      else { res.send(404, "No results"); }
+    });
+  });
+});
+
+
+// update bank time
+app.put('/api/banktime/:channelname/:username/:viewing_time', (req, res) => {
+  con.connect(function(err) {
+    if (err) throw err;
+    const sqlquery = `UPDATE ChannelViews SET viewing_time = ${req.params.viewing_time} WHERE channelname = '${req.params.channelname}' AND username = '${req.params.username}';`;
+    con.query(sqlquery, function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+      if (JSON.stringify(result) !== '[]') { res.send(result) }
+      else { res.send(404, "No results"); }
+    });
+  });
+});
+
+// start the server
+var server = app.listen(expressport, () => {
   // start db connection
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${expressport}`)
+  con.connect(function(err) {
+    if (err) throw err;
+    sql = "USE blossom_dev_db;"
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Result: " + JSON.stringify(result));
+    });
+    console.log("Connected to mysql db!");
+  });
 })
---
+++ setup_mysql_dev.sql
@@ -0,0 +1,17 @@
+-- prepares a MySQL server for the project
+
+CREATE DATABASE IF NOT EXISTS blossom_dev_db;
+CREATE USER IF NOT EXISTS 'blossom_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'blossom_dev_pwd';
+GRANT ALL PRIVILEGES ON `blossom_dev_db`.* TO 'blossom_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'blossom_dev'@'localhost';
+FLUSH PRIVILEGES;
+USE blossom_dev_db;
+
+CREATE table ChannelViews (
+   channelview_id INT NOT NULL AUTO_INCREMENT,
+   username VARCHAR(25) NOT NULL,
+   channelname VARCHAR(25) NOT NULL,
+   viewing_time INT NOT NULL DEFAULT 0,
+   banked_time INT NOT NULL DEFAULT 0,
+   PRIMARY KEY ( channelview_id )
+);
