ae31bcb5e70a542d511dbd0581dd5006936c67ef
AdamNB-sys <a1adnat@gmail.com>
2022-04-18 21:19:51 -0500

    Added mod/sub/broadcaster check to blossom add method

---
+++ moretest.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const ourObj = {"badge-info":{"subscriber":"14"},"badges":{"broadcaster":"1","subscriber":"0","premium":"1"},"color":"#8A2BE2","display-name":"MajorLoaf","emotes":null,"first-msg":false,"flags":null,"id":"ab0bb715-f139-4484-a693-c4f2c36c64ae","mod":false,"room-id":"609358561","subscriber":true,"tmi-sent-ts":"1650333129522","turbo":false,"user-id":"609358561","user-type":null,"emotes-raw":null,"badge-info-raw":"subscriber/14","badges-raw":"broadcaster/1,subscriber/0,premium/1","username":"majorloaf","message-type":"chat"}
+
+console.log(ourObj.badges.broadcaster)
